df2b92ab493370bff8c6aa644d027d496cd5ebd1
Noah Lee <17ngml@caslab.queensu.ca>
2021-01-17 11:31:21 -0500

    some comments to make code more readable, would add more if this was a team project

--- Image_Repo_Noah_Lee/user.h
+++ Image_Repo_Noah_Lee/user.h
@@ -22,14 +22,19 @@ struct product {
 
 class User {
 public:
+    //constructors
     User();
     User(string name);
     User(string name, string pword, float balance);
+    
+    //product and account manipulation
     void addProduct(string fileName, string name, string descriptors, float price);
     void purchaseProduct(string prodName);
     void deleteProduct(string prodName);
     void deposit(float amount);
     void search(string descriptors);
+    
+    //getters
     string getName(){ return this->username; }
     string getBalance(){
         stringstream bal;
@@ -43,10 +48,12 @@ private:
     vector<string> userInfo;
 };
 
+//helper functions to read and write information from text files
 void initiate();
 void initiate2();
 void save();
 
+//global containers to hold data and a pointer to the current user
 extern vector<product> DB;
 extern vector<string> userSave;
 extern User* loggedIn;
--- src/user.h
+++ src/user.h
@@ -7,35 +7,53 @@
 
 #include <string>
 #include <vector>
-
+#include <iomanip>
+#include <sstream>
+#pragma once
 using namespace std;
 
 struct product {
-	string User;
-	string name;
-	float price;
-	string imageFile;
-	string descriptors;
+    string User;
+    string name;
+    float price;
+    string imageFile;
+    string descriptors;
 };
 
 class User {
 public:
-	User();
-	User(string name);
-	User(string name, string pword, float balance);
-	void addProduct(string fileName, string name, string descriptors, float price);
-	void purchaseProduct(string prodName);
-	void deleteProduct(string prodName);
-	void deposit(float amount);
-	void search(string descriptors);
-	float balance;
+    //constructors
+    User();
+    User(string name);
+    User(string name, string pword, float balance);
+    
+    //product and account manipulation
+    void addProduct(string fileName, string name, string descriptors, float price);
+    void purchaseProduct(string prodName);
+    void deleteProduct(string prodName);
+    void deposit(float amount);
+    void search(string descriptors);
+    
+    //getters
+    string getName(){ return this->username; }
+    string getBalance(){
+        stringstream bal;
+        bal << std::fixed << setprecision(2) << balance;
+        return bal.str();
+    }
 private:
-	string username;
-	string pword;
-	vector<product> products;
-	//float balance;
-	vector<string> userInfo;
+    string username;
+    string pword;
+    float balance;
+    vector<string> userInfo;
 };
 
+//helper functions to read and write information from text files
+void initiate();
+void initiate2();
+void save();
+
+//global containers to hold data and a pointer to the current user
 extern vector<product> DB;
 extern vector<string> userSave;
+extern User* loggedIn;
